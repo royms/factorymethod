1bef847dc3bfffce5cbfad6fddc264fb960a6c6a
royms <hadash04@gmail.com>
2019-07-14 14:14:34 +0300

    ex2-factorymethod

---
+++ FactoryMethod/Sagway.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    class Sagway : Vehicle
+    {
+        protected string townvisiting;
+
+        public Sagway(string model, float cylinder, string color, int numberOfWheels, string townvisiting) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.townvisiting = townvisiting;
+        }
+
+        public override string ToString()
+        {
+            return "sagway : " + base.ToString() + $" { townvisiting}";
+        }
+    }
+}
---
+++ FactoryMethod/Truck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    public class Truck : Vehicle
+    {
+        protected string driversongs;
+
+        public Truck(string model, float cylinder, string color, int numberOfWheels, string driversongs) : base(model, cylinder, color, numberOfWheels)
+        {
+            this.driversongs = driversongs;
+        }
+
+        public override string ToString()
+        {
+            return "Truck : " + base.ToString() + $" { driversongs}";
+        }
+    }
+}
